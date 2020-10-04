e0b0c083196d1e905b684643af91233d258f79b6
Manan039 <56231590+Manan039@users.noreply.github.com>
2020-10-04 20:38:22 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$('#navbar a, .btn').on('click',function(event){
+    if(this.hash !==''){
+        event.preventDefault();
+        const hash = this.hash;
+        $('html,body').animate(
+            {
+                scrollTop: $(hash).offset().top -80
+            },
+            800
+        );
+    }
+});
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ''
+        }
+        else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5+ 0.1}s`;
+        }
+    });
+
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
